ae7ad88c73996653e22a7db62ff5279c06b394f5
Ole Bakman Borup <ole@reepay.com>
2020-01-03 14:49:08 +0100

    Experimentation

--- src/main/java/oleborup/sample/api/JerseyConfig.java
+++ src/main/java/oleborup/sample/api/JerseyConfig.java
@@ -2,8 +2,6 @@ package oleborup.sample.api;
 
 import oleborup.sample.api.resource.HelloResource;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.ApplicationPath;
@@ -12,8 +10,6 @@ import javax.ws.rs.ApplicationPath;
 @Component
 @ApplicationPath("/")
 public class JerseyConfig extends ResourceConfig {
-
-    private static final Logger LOG = LoggerFactory.getLogger(JerseyConfig.class);
 
     public JerseyConfig() {
         super();
